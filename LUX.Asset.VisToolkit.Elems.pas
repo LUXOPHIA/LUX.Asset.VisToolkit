b010e9577418f77a00c22c0ea56425227ae94381
luxidea <contact@luxidea.net>
2018-03-27 21:55:05 +0900

    LUX.Asset.VisToolkit.Elems / Model ユニットを追加。

---
+++ LUX.Asset.VisToolkit.Elems.pas
@@ -0,0 +1,362 @@
+﻿unit LUX.Asset.VisToolkit.Elems;
+
+interface //#################################################################### ■
+
+uses LUX, LUX.D1, LUX.D2, LUX.D3,
+     LUX.Asset.VisToolkit;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     TElemTypes = (
+       // Linear cells
+       VTK_EMPTY_CELL                       = 00,
+       VTK_VERTEX                           = 01,
+       VTK_POLY_VERTEX                      = 02,
+       VTK_LINE                             = 03,
+       VTK_POLY_LINE                        = 04,
+       VTK_TRIANGLE                         = 05,
+       VTK_TRIANGLE_STRIP                   = 06,
+       VTK_POLYGON                          = 07,
+       VTK_PIXEL                            = 08,
+       VTK_QUAD                             = 09,
+       VTK_TETRA                            = 10,
+       VTK_VOXEL                            = 11,
+       VTK_HEXAHEDRON                       = 12,
+       VTK_WEDGE                            = 13,
+       VTK_PYRAMID                          = 14,
+       VTK_PENTAGONAL_PRISM                 = 15,
+       VTK_HEXAGONAL_PRISM                  = 16,
+       // Quadratic, isoparametric cells
+       VTK_QUADRATIC_EDGE                   = 21,
+       VTK_QUADRATIC_TRIANGLE               = 22,
+       VTK_QUADRATIC_QUAD                   = 23,
+       VTK_QUADRATIC_POLYGON                = 36,
+       VTK_QUADRATIC_TETRA                  = 24,
+       VTK_QUADRATIC_HEXAHEDRON             = 25,
+       VTK_QUADRATIC_WEDGE                  = 26,
+       VTK_QUADRATIC_PYRAMID                = 27,
+       VTK_BIQUADRATIC_QUAD                 = 28,
+       VTK_TRIQUADRATIC_HEXAHEDRON          = 29,
+       VTK_QUADRATIC_LINEAR_QUAD            = 30,
+       VTK_QUADRATIC_LINEAR_WEDGE           = 31,
+       VTK_BIQUADRATIC_QUADRATIC_WEDGE      = 32,
+       VTK_BIQUADRATIC_QUADRATIC_HEXAHEDRON = 33,
+       VTK_BIQUADRATIC_TRIANGLE             = 34,
+       // Cubic, isoparametric cell
+       VTK_CUBIC_LINE                       = 35,
+       // Special class of cells formed by convex group of points
+       VTK_CONVEX_POINT_SET                 = 41,
+       // Polyhedron cell (consisting of polygonal faces)
+       VTK_POLYHEDRON                       = 42,
+       // Higher order cells in parametric form
+       VTK_PARAMETRIC_CURVE                 = 51,
+       VTK_PARAMETRIC_SURFACE               = 52,
+       VTK_PARAMETRIC_TRI_SURFACE           = 53,
+       VTK_PARAMETRIC_QUAD_SURFACE          = 54,
+       VTK_PARAMETRIC_TETRA_REGION          = 55,
+       VTK_PARAMETRIC_HEX_REGION            = 56,
+       // Higher order cells
+       VTK_HIGHER_ORDER_EDGE                = 60,
+       VTK_HIGHER_ORDER_TRIANGLE            = 61,
+       VTK_HIGHER_ORDER_QUAD                = 62,
+       VTK_HIGHER_ORDER_POLYGON             = 63,
+       VTK_HIGHER_ORDER_TETRAHEDRON         = 64,
+       VTK_HIGHER_ORDER_WEDGE               = 65,
+       VTK_HIGHER_ORDER_PYRAMID             = 66,
+       VTK_HIGHER_ORDER_HEXAHEDRON          = 67,
+       // Arbitrary order Lagrange elements (formulated separated from generic higher order cells)
+       VTK_LAGRANGE_CURVE                   = 68,
+       VTK_LAGRANGE_TRIANGLE                = 69,
+       VTK_LAGRANGE_QUADRILATERAL           = 70,
+       VTK_LAGRANGE_TETRAHEDRON             = 71,
+       VTK_LAGRANGE_HEXAHEDRON              = 72,
+       VTK_LAGRANGE_WEDGE                   = 73,
+       VTK_LAGRANGE_PYRAMID                 = 74,
+       VTK_NUMBER_OF_CELL_TYPES                   );
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell
+
+     TvtkCell = class( TvtkElem )
+     private
+     protected
+       _Kind  :TElemTypes;
+       _Poins :TArray<TvtkPoin>;
+       ///// アクセス
+       function GetPoins( const I_:Integer ) :TvtkPoin;
+       procedure SetPoins( const I_:Integer; const Poin_:TvtkPoin );
+     public
+       constructor Create( const PoinsN_:Integer );
+       destructor Destroy; override;
+       ///// プロパティ
+       property Kind                      :TElemTypes read   _Kind  write   _Kind ;
+       property Poins[ const I_:Integer ] :TvtkPoin   read GetPoins write SetPoins;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell01
+
+     TvtkCell01 = class( TvtkCell )
+     private
+     protected
+     public
+       constructor Create;
+       destructor Destroy; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell05
+
+     TvtkCell05 = class( TvtkCell )
+     private
+     protected
+     public
+       constructor Create;
+       destructor Destroy; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell09
+
+     TvtkCell09 = class( TvtkCell )
+     private
+     protected
+     public
+       constructor Create;
+       destructor Destroy; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell10
+
+     TvtkCell10 = class( TvtkCell )
+     private
+     protected
+     public
+       constructor Create;
+       destructor Destroy; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell12
+
+     TvtkCell12 = class( TvtkCell )
+     private
+     protected
+     public
+       constructor Create;
+       destructor Destroy; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell13
+
+     TvtkCell13 = class( TvtkCell )
+     private
+     protected
+     public
+       constructor Create;
+       destructor Destroy; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell14
+
+     TvtkCell14 = class( TvtkCell )
+     private
+     protected
+     public
+       constructor Create;
+       destructor Destroy; override;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+uses System.SysUtils, System.Classes, System.RegularExpressions;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+function TvtkCell.GetPoins( const I_:Integer ) :TvtkPoin;
+begin
+     Result := _Poins[ I_ ];
+end;
+
+procedure TvtkCell.SetPoins( const I_:Integer; const Poin_:TvtkPoin );
+begin
+     _Poins[ I_ ] := Poin_;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TvtkCell.Create( const PoinsN_:Integer );
+begin
+     inherited Create;
+
+     SetLength( _Poins, PoinsN_ );
+end;
+
+destructor TvtkCell.Destroy;
+begin
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell01
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TvtkCell01.Create;
+begin
+     inherited Create( 1 );
+
+end;
+
+destructor TvtkCell01.Destroy;
+begin
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell05
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TvtkCell05.Create;
+begin
+     inherited Create( 3 );
+
+end;
+
+destructor TvtkCell05.Destroy;
+begin
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell09
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TvtkCell09.Create;
+begin
+     inherited Create( 4 );
+
+end;
+
+destructor TvtkCell09.Destroy;
+begin
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell10
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TvtkCell10.Create;
+begin
+     inherited Create( 4 );
+
+end;
+
+destructor TvtkCell10.Destroy;
+begin
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell12
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TvtkCell12.Create;
+begin
+     inherited Create( 8 );
+
+end;
+
+destructor TvtkCell12.Destroy;
+begin
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell13
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TvtkCell13.Create;
+begin
+     inherited Create( 6 );
+
+end;
+
+destructor TvtkCell13.Destroy;
+begin
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TvtkCell14
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TvtkCell14.Create;
+begin
+     inherited Create( 5 );
+
+end;
+
+destructor TvtkCell14.Destroy;
+begin
+
+     inherited;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+//############################################################################## □
+
+initialization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 初期化
+
+finalization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 最終化
+
+end. //######################################################################### ■
---
+++ LUX.Asset.VisToolkit.Model.pas
@@ -0,0 +1,253 @@
+﻿unit LUX.Asset.VisToolkit.Model;
+
+interface //#################################################################### ■
+
+uses System.Generics.Collections,
+     LUX, LUX.D1, LUX.D2, LUX.D3,
+     LUX.Asset.VisToolkit,
+     LUX.Asset.VisToolkit.Elems;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVisToolkit
+
+     TVisToolkit = class
+     private
+     protected
+       _Version  :String;
+       _Caption  :String;
+       _FileType :String;
+       _DataType :String;
+       _Poins    :TObjectList<TvtkPoin>;
+       _Cells    :TObjectList<TvtkCell>;
+     public
+       constructor Create;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Version  :String                read _Version ;
+       property Caption  :String                read _Caption ;
+       property FileType :String                read _FileType;
+       property DataType :String                read _DataType;
+       property Poins    :TObjectList<TvtkPoin> read _Poins   ;
+       property Cells    :TObjectList<TvtkCell> read _Cells   ;
+       ///// メソッド
+       procedure LoadFromFile( const FileName_:String );
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+uses System.SysUtils, System.Classes, System.RegularExpressions;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVisToolkit
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TVisToolkit.Create;
+begin
+     inherited;
+
+     _Poins := TObjectList<TvtkPoin>.Create;
+     _Cells := TObjectList<TvtkCell>.Create;
+end;
+
+destructor TVisToolkit.Destroy;
+begin
+     _Poins.DisposeOf;
+     _Cells.DisposeOf;
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TVisToolkit.LoadFromFile( const FileName_:String );
+var
+   F :TStreamReader;
+   Vs :TArray<String>;
+//······································
+     function GetLine :String;
+     begin
+          while not F.EndOfStream do
+          begin
+               Result := F.ReadLine;
+
+               if Result <> '' then Break;
+          end;
+     end;
+     //·································
+     procedure ReadValues( const VsN_:Integer );
+     begin
+          Vs := [];
+
+          while Length( Vs ) < VsN_ do Vs := Vs + GetLine.Split( [ ' ' ] );
+     end;
+//······································
+var
+   R :TRegEx;
+   PsN, I, CsN, VsN, EsN, J, N :Integer;
+   PsT :String;
+   P :TSingle3D;
+   Poin :TvtkPoin;
+   Cell :TvtkCell;
+begin
+     F := TStreamReader.Create( FileName_, TEncoding.Default );
+     try
+          //////////
+
+          R := TRegEx.Create( '# +vtk +DataFile +Version +([^.]+\.[^.]+)' );
+
+          with R.Match( GetLine ) do
+          begin
+               Assert( Success, '# vtk DataFile Version' );
+
+               _Version := Groups[ 1 ].Value;
+          end;
+
+          //////////
+
+          _Caption := GetLine;
+
+          //////////
+
+          _FileType := GetLine;  Assert( _FileType = 'ASCII', '_FileType = ' + _FileType );
+
+          //////////
+
+          R := TRegEx.Create( 'DATASET +([^ ]+)' );
+
+          with R.Match( GetLine ) do
+          begin
+               Assert( Success, 'DATASET' );
+
+               _DataType := Groups[ 1 ].Value;
+          end;
+
+          Assert( _DataType = 'UNSTRUCTURED_GRID', '_DataType = ' + _DataType );
+
+          //////////
+
+          R := TRegEx.Create( 'POINTS +([^ ]+) +([^ ]+)' );
+
+          with R.Match( GetLine ) do
+          begin
+               Assert( Success, 'POINTS' );
+
+               PsN := Groups[ 1 ].Value.ToInteger;
+               PsT := Groups[ 2 ].Value;
+          end;
+
+          ReadValues( 3 * PsN );
+
+          _Poins.Clear;
+
+          I := 0;
+
+          for N := 1 to PsN do
+          begin
+               Poin := TvtkPoin.Create;
+
+               P.X := Vs[ I ].ToSingle;  Inc( I );
+               P.Y := Vs[ I ].ToSingle;  Inc( I );
+               P.Z := Vs[ I ].ToSingle;  Inc( I );
+
+               with Poin do
+               begin
+                    ID := _Poins.Count;
+
+                    Pos := P;
+               end;
+
+               _Poins.Add( Poin );
+          end;
+
+          //////////
+
+          R := TRegEx.Create( 'CELLS +([^ ]+) +([^ ]+)' );
+
+          with R.Match( GetLine ) do
+          begin
+               Assert( Success, 'CELLS' );
+
+               CsN := Groups[ 1 ].Value.ToInteger;
+               VsN := Groups[ 2 ].Value.ToInteger;
+          end;
+
+          ReadValues( VsN );
+
+          _Cells.Clear;
+
+          I := 0;
+
+          for N := 1 to CsN do
+          begin
+               PsN := Vs[ I ].ToInteger;  Inc( I );
+
+               Cell := TvtkCell.Create( PsN );
+
+               with Cell do
+               begin
+                    ID := _Cells.Count;
+
+                    for J := 0 to PsN-1 do
+                    begin
+                         Poins[ J ] := Self._Poins[ Vs[ I ].ToInteger ];  Inc( I );
+                    end;
+               end;
+
+               _Cells.Add( Cell );
+          end;
+
+          //////////
+
+          R := TRegEx.Create( 'CELL_TYPES +([^ ]+)' );
+
+          with R.Match( GetLine ) do
+          begin
+               Assert( Success, 'CELL_TYPES' );
+
+               CsN := Groups[ 1 ].Value.ToInteger;
+          end;
+
+          ReadValues( CsN );
+
+          for I := 0 to CsN-1 do _Cells[ I ].Kind := TElemTypes( Vs[ I ].ToInteger );
+
+          //////////
+
+          //////////
+
+          //////////
+
+
+     finally
+            F.DisposeOf;
+     end;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+//############################################################################## □
+
+initialization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 初期化
+
+finalization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 最終化
+
+end. //######################################################################### ■
